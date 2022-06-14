79126d83d234e828fb77d574039966444e0d6afe
Egemen <egemendurmuss@gmail.com>
2022-06-14 16:56:03 -0400

    added signup function

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import TutorialDataService from "../TutorialDataService";
 
 export default function Signup(){
+
+
+    function sendForm(){
+        console.log("button clicked.")
+        if(!(firstName.value&&lastName.value&&password.value&&password_con.value&&email.value))
+        return console.log("Empty fields")
+        else if(password.value!==password_con.value)
+        return console.log("Passwords don't match")
+        else{
+    return TutorialDataService.createUser({"firstName":firstName.value,"lastName":lastName.value,"email":email.value,"password":password.value,"summary":""})}
+    }
+    let firstName = document.getElementById("fname");
+    let lastName = document.getElementById("lname");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let password_con = document.getElementById("password-con");
+
+
     return(<div><h1 style={{"textAlign":"center"}}>Signup Form</h1>
     <form>
 	<label for="fname">First Name</label>
@@ -13,7 +31,7 @@ export default function Signup(){
 <label for="password">Password</label>
 <input type="password" placeholder=" Enter Password" id="password" name="password"/>
 <input type="password" placeholder="Confirm Password" id="password-con" name="password-con"/>
-<input type="submit" className="submit-btn" name="Submit" value="Signup"/>
+<div className="submit-btn" name="Submit" value="Signup" onClick={sendForm}>Submit</div>
 </form></div>)
 
 }
